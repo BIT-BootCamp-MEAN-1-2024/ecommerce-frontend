a02006f79234c9990e22a7994d117a618017aafa
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2024-06-14 17:09:47 -0500

    [+] products.component.*: Lista todos los productos registrados

--- src/app/private/pages/products/products.component.ts
+++ src/app/private/pages/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+  /** Atributos */
+  products: any;
 
+  constructor( private productService: ProductsService ) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe( data => {
+      console.log( data );
+      this.products = data.data;
+    });
+  }
 }
